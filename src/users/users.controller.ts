24b9b34d561fc05998e2ed606c79023c890e0358
ewerton.marschalk <ewerton.marschalk@redeunifique.com.br>
2022-04-08 15:03:37 -0300

    finisnhing user crud routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { get } from 'http';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 
@@ -28,4 +28,15 @@ export class UsersController {
     findUsersByEmail(@Query('email') email: string) {
         return this.userService.findByEmail(email);
     }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id: string){
+        return this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+        return this.userService.update(parseInt(id), body);
+    }
+
 }
