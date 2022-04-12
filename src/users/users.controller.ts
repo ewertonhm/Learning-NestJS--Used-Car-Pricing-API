5dbedc09fc442ba8cdf78132ec9adb04678ab434
Ewerton Henrique Marschalk <you@example.com>
2022-04-12 08:00:09 -0300

    refactor to make the interceptor reusable

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { SerializeInterceptor } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 
 @Controller('auth')
@@ -15,13 +16,13 @@ export class UsersController {
         this.userService.create(body.email, body.password);
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/list-users')
     findAllUsers(){
         return this.userService.list();
     }
 
-    @UseInterceptors(SerializeInterceptor)
+    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user =  await this.userService.findOne(parseInt(id));
