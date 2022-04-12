62c17b2c35b305c460be3fc45f7571f791a8cd0a
Ewerton Henrique Marschalk <you@example.com>
2022-04-12 13:30:02 -0300

    sessions setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const CookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CookieSession({
+    keys: ['a2dqdd2d23rf3212asda3'],
+  }))
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -13,13 +13,17 @@ export class UsersController {
     constructor(private userService: UsersService, private authService: AuthService){}
 
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-        return this.authService.signup(body.email, body.password);
+    async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signup(body.email, body.password);
+        session.userid = user.id;
+        return user;
     }
 
     @Post('/signin')
-    singnin(@Body() body: CreateUserDto) {
-        return this.authService.signin(body.email, body.password);
+    async singnin(@Body() body: CreateUserDto, @Session() session: any) {
+        const user = await this.authService.signin(body.email, body.password);
+        session.userid = user.id;
+        return user;
     }
 
     @Get('/list-users')
